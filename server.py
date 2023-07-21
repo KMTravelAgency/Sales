014e0efcbd2308ed7afc7ea80f3d66378773d9a7
KennetTech <64315533+KennetTech@users.noreply.github.com>
2023-07-21 14:46:22 +0200

    setting up consumption of messages from rabbitmq

--- server.py
+++ server.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
+from rabbitmq_helper import RabbitMQHelper
+import json
 
 app = FastAPI()
+
+def callback(ch, method, properties, body):
+
+    body =  json.loads(body.decode('utf-8'))
+
+    print(body)
+    
+def main():
+    RabbitMQHelper.consume_message(callback = callback)
+
+@app.on_event("startup")
+def startup_event():
+    main()
 
 @app.get("/")
 def root():
